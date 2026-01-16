c2248e9a774a8bad7c15534c75a2c4e4f7a97d5d
MircoPerna <132653588+MircoPerna99@users.noreply.github.com>
2026-01-16 22:05:41 +0100

    Completed project

--- Model.py
+++ Model.py
@@ -144,13 +144,14 @@ if len(devices) > 0:
     tf.config.set_visible_devices(devices[0], 'GPU')
     tf.config.experimental.set_memory_growth(devices[0], True)
 else:
-    print("GPU non trovata.")
+    print("GPU has not been found.")
     
 cross_validation_results=[]
 learning_rates = [0.01,0.001]
 momentums = [0.9,0.5]
 kernel_sizes = [2,3,5]
 
+#TUNING
 for dataset_type in DatasetTypes:
   X, y= load_data(dataset_type)
   X_train, X_test,y_train,y_test = train_test_split(X,y, test_size=0.3)
@@ -170,6 +171,7 @@ for dataset_type in DatasetTypes:
           cross_validation_results.append(details)
          
 df = pd.DataFrame(cross_validation_results, columns = ['model_type', 'dataset_type', 'learning_rate', 'momentum', 'kernel_size', 'accuracy_mean', 'standard_deviation']) 
+#SAVE RESULTS
 if(APPLY_DECAY):
   df.to_csv('result_with_decay.csv', index=False)  
 else:
